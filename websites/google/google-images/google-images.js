b2078d48bbfba41c9bdad7e05df3bdea5bd53a24
Sahdeek <sahdeek.thompson@gmail.com>
2022-06-07 02:54:12 -0500

    initial commit

---
+++ websites/google/google-images/google-images.js
@@ -0,0 +1,7 @@
+console.log("portrait compat for google images")
+
+// const imgViewer = document.getElementById("islsp")
+// imgViewer.classList.add("img-viewer-portrait-mode")
+
+// const imgList = document.getElementById("islmp")
+// imgList.classList.add("img-list-portrait-mode")
